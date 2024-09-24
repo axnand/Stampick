ed499b9e510268db999a6d1d6f5c7e81c8fd4673
saurav kumar <sauravkumar8887252285@gmail.com>
2024-09-25 00:35:42 +0530

    Dashboard completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['randomuser.me', 'assets.aceternity.com'], // Add the external domains here
+//   },
+ 
+// };
+
+// export default nextConfig;
+
+
+  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
